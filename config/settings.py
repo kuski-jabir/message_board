0e04e91866caf93efe56279f3e54a6337ac39f41
kuski <kuskihaji@gmail.com>
2021-04-26 20:57:17 +0300

    Setting allowed hosts for Heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ SECRET_KEY = "*iba#@tcyk3_okhyfekfyzbcj=6a*slw(fp+37$kw1ir$b#0%3"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
